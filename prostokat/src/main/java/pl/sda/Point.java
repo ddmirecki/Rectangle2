c62a19f65fc95d8a2918886c69336b650367a728
Daniel <ddmirecki@gmail.com>
2017-01-08 14:37:22 +0100

    drugi

---
+++ prostokat/src/main/java/pl/sda/Point.java
@@ -0,0 +1,13 @@
+package pl.sda;
+
+/**
+ * Created by Daniel on 2017-01-08.
+ */
+public class Point {
+    int x;
+    int y;
+
+    public Point(int x, int y){
+
+    }
+}
---
+++ prostokat/src/main/java/pl/sda/Rectangle.java
@@ -0,0 +1,7 @@
+package pl.sda;
+
+/**
+ * Created by Daniel on 2017-01-08.
+ */
+public class Rectangle {
+}
