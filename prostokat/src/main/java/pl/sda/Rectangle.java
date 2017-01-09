4da5d1e3deb5764034bb4dedfb5a824fde03c728
Daniel <ddmirecki@gmail.com>
2017-01-09 14:56:24 +0100

    drugi

--- prostokat/src/main/java/pl/sda/Rectangle.java
+++ prostokat/src/main/java/pl/sda/Rectangle.java
@@ -6,8 +6,8 @@ package pl.sda;
 public class Rectangle {
     Rectangle rectangle1 = new Rectangle();
     Point point1 = new Point(0, 0);
-    Point point2 = new Point(0, 5);
-    Point point3 = new Point(0, 10);
+    Point point2 = new Point(10, 0);
+    Point point3 = new Point(0, 5);
     Point point4 = new Point(5, 10);
 
     Rectangle rectangle2 = new Rectangle();
@@ -15,6 +15,8 @@ public class Rectangle {
     Point point2a = new Point(2, 12);
     Point point3a = new Point(2, 7);
     Point point4a = new Point(7, 12);
+
+
 
 
 
